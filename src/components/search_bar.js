277013992916c901729fe5c0e21b80ac32825afa
Bobby Nijjar <bobby951@gmail.com>
2016-11-21 18:17:29 -0500

    added styles and throttled search using lodash

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component {
 
     render() {
       return (
-        <div className="search_bar">
+        <div className="search-bar">
           <input
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
